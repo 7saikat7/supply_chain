b449d3f1c79aa8fde5fefd5b98c686630b4a0eb5
7saikat7 <7saikat7@gmail.com>
2022-01-31 23:08:04 +0530

    changed authentication

--- Supply_Chain/Supply_Chain/settings.py
+++ Supply_Chain/Supply_Chain/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth',
+
     'dashboard',
     'inventory',
     'payment',
