7cc88f824c48100f4e46ebc37d9068a7c0e33639
RitikaSingh02 <ritika2002singh@gmail.com>
2022-02-03 00:32:38 +0530

    --add models and views for dashboard

--- Supply_Chain/dashboard/models.py
+++ Supply_Chain/dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class QueryType(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length= 200 , default="NULL")
+
+class ContactInfo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length= 200 , default="NULL")
+    phone_no = models.CharField(max_length= 200 , default="NULL" , unique=True)
+    email = models.EmailField(max_length=200 , unique=True)
+    type_of_query = models.ForeignKey(QueryType, on_delete=models.CASCADE , related_name='query_type')
+    description = models.CharField(max_length= 200 , default="NULL")
+
--- Supply_Chain/dashboard/views.py
+++ Supply_Chain/dashboard/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import ContactSerializer
+from .models import ContactInfo
+
+@api_view(['GET'])
+def contact_list(request):
+    q = ContactInfo.objects.all()
+    serializer = ContactSerializer(q , many = True)
+    return Response(serializer.data , status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def contact_detail(request):
+    obj_id = request.GET.get('id')
+    contact = ContactInfo.objects.get(id = obj_id)
+    serializer = ContactSerializer(contact , many = False)
+    return Response(serializer.data ,  status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def contact_create(request):
+    res = {}
+    serializer = ContactSerializer(data = request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    res['msg'] = "Contact info created successfully"
+    res['data'] = serializer.data
+    return Response(res, status=status.HTTP_201_CREATED)
+    
+@api_view(['POST'])
+def contact_update(request):
+    res={}
+    body = request.data
+    curr_id = body['id']
+    q = ContactInfo.objects.get(id = curr_id)
+    serializer = ContactSerializer(instance = q ,data = body)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    res['msg'] = "Contact info updated successfully"
+    res['data'] = serializer.data
+    return Response(res, status=status.HTTP_200_OK)
