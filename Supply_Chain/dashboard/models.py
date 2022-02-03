f3d03fef537088437c6f2cc4ffbbfc430145ff9a
RitikaSingh02 <ritika2002singh@gmail.com>
2022-02-03 15:38:14 +0530

    removed unique check for phone number

--- Supply_Chain/dashboard/models.py
+++ Supply_Chain/dashboard/models.py
@@ -8,7 +8,7 @@ class QueryType(models.Model):
 class ContactInfo(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length= 200 , default="NULL")
-    phone_no = models.CharField(max_length= 200 , default="NULL" , unique=True)
+    phone_no = models.CharField(max_length= 200 , default="NULL" , unique = False)
     email = models.EmailField(max_length=200 , unique=True)
     type_of_query = models.ForeignKey(QueryType, on_delete=models.CASCADE , related_name='query_type')
     description = models.CharField(max_length= 200 , default="NULL")
