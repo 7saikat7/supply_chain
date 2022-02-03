aca40a8bffb72aae7ee68d2ad69c5913909b88a6
RitikaSingh02 <ritika2002singh@gmail.com>
2022-02-03 10:07:41 +0530

    --add serializer for QueryType table

--- Supply_Chain/dashboard/serializers.py
+++ Supply_Chain/dashboard/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import ContactInfo
+from .models import ContactInfo , QueryType
 
 class ContactSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = ContactInfo
+		fields ='__all__'
+
+class QueryTypeSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = QueryType
 		fields ='__all__'
