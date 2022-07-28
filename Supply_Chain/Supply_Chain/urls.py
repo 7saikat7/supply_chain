6ae84d07978a5dd16b112aa537f65fe24967ee92
saikat <saikat@moment.club>
2022-07-28 13:10:44 +0530

    seperated front end

--- Supply_Chain/Supply_Chain/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('dashboard/', include('dashboard.urls') ),
-]
