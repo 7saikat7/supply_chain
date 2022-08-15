1dfab1c835d387309debda55603fccf02cc82ec4
Srishti Ahuja <sam.srishtiahuja@gmail.com>
2022-08-16 00:34:36 +0530

    Custom User Model

--- Supply_Chain/authentication/manager.py
+++ Supply_Chain/authentication/manager.py
@@ -3,18 +3,18 @@ from django.contrib.auth.base_user import BaseUserManager
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def create_user(self,username,password=None, **extra_fields):
-        if not username:
-            raise ValueError('The given username must be set')
+    def create_user(self,email,password=None, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
         if not password:
             raise ValueError('The given password must be set')
         
-        user = self.model(username=username, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self,username,password,**extra_fields):
+    def create_superuser(self,email,password,**extra_fields):
         extra_fields.setdefault('is_staff',True)
         extra_fields.setdefault('is_superuser',True)
         extra_fields.setdefault('is_active',True)
@@ -22,4 +22,4 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Super User must have is_staff=True')
 
-        return self.create_user(username,password,**extra_fields)
+        return self.create_user(email,password,**extra_fields)
